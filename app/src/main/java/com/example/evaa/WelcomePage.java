d153745a707362e8f87e4095eeeb94b4fae1b6b3
Ellise Limjoco <elliselimjoco28@yahoo.com>
2020-07-22 23:49:34 -0700

    Updated fonts, removed FunFacts
    oswald.xml works as an AppTheme. Re-added slide animation between WelcomePage and Options. Removed FunFacts.java and fun_facts.xml.

--- app/src/main/java/com/example/evaa/WelcomePage.java
+++ app/src/main/java/com/example/evaa/WelcomePage.java
@@ -1,9 +1,14 @@
 package com.example.evaa;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -23,18 +28,25 @@ public class WelcomePage extends AppCompatActivity {
         llTap = findViewById(R.id.llTap);
         ivEarth = findViewById(R.id.ivEarth);
         tvWelcome = findViewById(R.id.tvWelcome);
+        final Typeface typeface = ResourcesCompat.getFont(this, R.font.oswald);
+        tvWelcome.setTypeface(typeface);
 
         llTap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Intent intent = new Intent(WelcomePage.this,
-                        FunFacts.class);
-
-                startActivity(intent);
-
+                slideTransition();
             }
         });
+    }
 
+    public void slideTransition() {
+        llTap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomePage.this, com.example.evaa.Options.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slidein_right, R.anim.slideout_left);
+            }
+        });
     }
 }
