346b2b5188a0b401b3c7d20c9dfad28c933defc1
Angeline Pho <apho8884@gmail.com>
2020-07-25 13:22:15 -0700

    RecyclerView doesn't work

--- app/src/main/java/com/example/evaa/Environment.java
+++ app/src/main/java/com/example/evaa/Environment.java
@@ -1,14 +1,4 @@
 package com.example.evaa;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-public class Environment extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.environment_page);
-    }
+public class Environment {
 }
--- app/src/main/java/com/example/evaa/Options.java
+++ app/src/main/java/com/example/evaa/Options.java
@@ -1,48 +1,4 @@
 package com.example.evaa;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-public class Options extends AppCompatActivity {
-
-    TextView tvInstruction;
-    Button btnItemSearch, btnEnvironment;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.options);
-
-        tvInstruction = findViewById(R.id.tvInstruction);
-        btnItemSearch = findViewById(R.id.btnItemSearch);
-        btnEnvironment = findViewById(R.id.btnEnvironment);
-
-        btnItemSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(Options.this,
-                        ItemsPage.class);
-
-                startActivity(intent);
-            }
-        });
-
-        btnEnvironment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(Options.this,
-                        Environment.class);
-
-                startActivity(intent);
-
-            }
-        });
-    }
+public class Options {
 }
