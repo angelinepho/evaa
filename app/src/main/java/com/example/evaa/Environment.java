8051e9f4662034e6b8339d00cfdcb126d7b2647e
Ellise Limjoco <elliselimjoco28@yahoo.com>
2020-07-26 02:23:00 -0700

    Added progress bar to Environment
    Created custom_progressbar.xml. Added to Environment. Still need to implement measure of progress.

--- app/src/main/java/com/example/evaa/Environment.java
+++ app/src/main/java/com/example/evaa/Environment.java
@@ -2,13 +2,40 @@ package com.example.evaa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class Environment extends AppCompatActivity {
+
+    private ProgressBar progressBarAnim;
+    private ObjectAnimator progressAnimator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.environment_page);
+        initializeProgressBar();
+        progressAnimator.setDuration(7000);
+
+        progressAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator anim) {
+                super.onAnimationEnd(anim);
+                Toast.makeText(getBaseContext(), "Completed", Toast.LENGTH_SHORT).show();
+                progressBarAnim.setVisibility(View.GONE);
+            }
+        });
+
     }
+
+    private void initializeProgressBar() {
+        progressBarAnim = findViewById(R.id.progress_horizontal);
+        progressAnimator = ObjectAnimator.ofInt(progressBarAnim, "progress", 0, 100);
+    }
+
 }
