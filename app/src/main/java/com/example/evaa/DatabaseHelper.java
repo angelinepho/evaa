c3061abb1e23c36db7d00d70d27c1bb3bfff127c
Angeline Pho <apho8884@gmail.com>
2020-07-27 19:39:13 -0700

    SQLiteDatabase setup to log items

---
+++ app/src/main/java/com/example/evaa/DatabaseHelper.java
@@ -0,0 +1,59 @@
+package com.example.evaa;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String TABLE_NAME = "item_table";
+    private static final String COL0 = "ID";
+    private static final String COL1 = "item";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, TABLE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL1 + " TEXT)";
+        sqLiteDatabase.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+    public boolean addData(String item) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL1, item);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Returns all the data from database
+     * @return
+     */
+    public Cursor getData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_NAME;
+        Cursor data = db.rawQuery(query, null);
+        return data;
+    }
+
+}
--- app/src/main/java/com/example/evaa/SelectedItem.java
+++ app/src/main/java/com/example/evaa/SelectedItem.java
@@ -4,19 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import es.dmoral.toasty.Toasty;
 
 
 public class SelectedItem extends AppCompatActivity {
+
+    DatabaseHelper mDatabaseHelper;
+    private Button btnAdd;
+    private TextView image_description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.selected_item);
 
+        btnAdd = findViewById(R.id.btnAdd);
+        mDatabaseHelper = new DatabaseHelper(this);
+        image_description = findViewById(R.id.image_description);
+
         getIncomingIntent();
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = image_description.getText().toString();
+                if (name.length() != 0) {
+                    AddData(name);
+                } else {
+                    Toasty.warning(SelectedItem.this, "No Item to Log", Toast.LENGTH_SHORT, true).show();
+                }
+            }
+        });
+    }
+
+    public void AddData(String newEntry) {
+        boolean insertData = mDatabaseHelper.addData(newEntry);
+
+        if (insertData) {
+            Toasty.success(SelectedItem.this, "Log Success", Toast.LENGTH_SHORT, true).show();
+        } else {
+            Toasty.error(SelectedItem.this, "Log Error", Toast.LENGTH_SHORT, true).show();
+        }
     }
 
     private void getIncomingIntent(){
