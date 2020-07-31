ab1cf3736a72d69b5417c18875aab16359d53326
Ellise Limjoco <elliselimjoco28@yahoo.com>
2020-07-30 23:29:26 -0700

    LoggedList activity updates upon logging
    Updated DatabaseHelper.java to fit needs. Linked SelectedItems.java input to output on LoggedList.java. LoggedList.java now displays Item Name.

--- app/src/main/java/com/example/evaa/DatabaseHelper.java
+++ app/src/main/java/com/example/evaa/DatabaseHelper.java
@@ -5,26 +5,29 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    private static final String TABLE_NAME = "item_table";
-    private static final String COL0 = "ID";
-    private static final String COL1 = "item";
+    private static final String TABLE_NAME = "ITEMS_TABLE";
+    private static final String COLUMN_ITEM_NAME = "ITEM_NAME";
 
     public DatabaseHelper(@Nullable Context context) {
-        super(context, TABLE_NAME, null, 1);
+        super(context, "item_table.db", null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                COL1 + " TEXT)";
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_ITEM_NAME + " TEXT)";
         sqLiteDatabase.execSQL(createTable);
     }
 
+    // Method for if the database version number changes
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
@@ -32,11 +35,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     public boolean addData(String item) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(COL1, item);
 
-        long result = db.insert(TABLE_NAME, null, contentValues);
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_ITEM_NAME, item);
+
+        long result = db.insert(TABLE_NAME, null, cv);
 
         if (result == -1) {
             return false;
@@ -45,15 +49,29 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
     }
 
-    /**
-     * Returns all the data from database
-     * @return
-     */
-    public Cursor getData(){
-        SQLiteDatabase db = this.getWritableDatabase();
+    public List<String> getData(){
+        List<String> returnList = new ArrayList<>();
+
         String query = "SELECT * FROM " + TABLE_NAME;
-        Cursor data = db.rawQuery(query, null);
-        return data;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(query, null);
+        if (cursor.moveToFirst()) {
+            // Loop through the cursor
+            do {
+                String itemName = cursor.getString(1);
+                returnList.add(itemName);
+            } while (cursor.moveToNext());
+        } else {
+            // Do not add anything to the list
+        }
+
+        // Close both the cursor and the db
+        cursor.close();
+        db.close();
+        return returnList;
     }
+
+
 
 }
--- app/src/main/java/com/example/evaa/LoggedList.java
+++ app/src/main/java/com/example/evaa/LoggedList.java
@@ -3,22 +3,31 @@ package com.example.evaa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoggedList extends AppCompatActivity {
 
     private ImageButton btnExit;
-    ListView listLogged;
+    private ListView listLogged;
+    private List<String> displayList = new ArrayList();
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_list);
         btnExit = findViewById(R.id.btnExit);
-        listLogged = findViewById(R.id.listLogged);
+        listLogged = (ListView) findViewById(R.id.listLogged);
+
         btnExit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,5 +36,16 @@ public class LoggedList extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        DatabaseHelper databaseHelper = new DatabaseHelper(LoggedList.this);
+        displayList = databaseHelper.getData();
+        displayList.add("Orange");
+
+
+        adapter = new ArrayAdapter<String>(LoggedList.this, android.R.layout.simple_list_item_1, displayList);
+        listLogged.setAdapter(adapter);
     }
+
+
+
 }
