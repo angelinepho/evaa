c443e7d37b46af4a8e73114686e05de49b735d5f
Angeline Pho <apho8884@gmail.com>
2020-08-06 12:01:12 -0700

    sldie transitions

--- app/src/main/java/com/example/evaa/SearchFragment.java
+++ app/src/main/java/com/example/evaa/SearchFragment.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.transition.TransitionInflater;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
